ddfb8e32e4aff189f85b9dd9d14e86d2424a5ff0
arabindasantara12-ops <arabindasantara12@gmail.com>
2026-07-30 22:03:53 +0530

    Create finder.php

---
+++ includes/templates/finder.php
@@ -0,0 +1,24 @@
+<div class="tapf-container">
+
+    <div class="tapf-hero">
+        <h1>📱 Tech Arabinda Phone Finder Pro</h1>
+
+        <p>
+            Search smartphones instantly and compare specifications.
+        </p>
+    </div>
+
+    <div class="tapf-search-box">
+
+        <input
+            type="text"
+            id="tapf-search"
+            placeholder="Search Samsung, iPhone, OnePlus..."
+
+        >
+
+    </div>
+
+    <div id="tapf-result"></div>
+
+</div>
